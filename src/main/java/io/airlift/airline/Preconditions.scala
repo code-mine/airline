c8e6f94a9dd156dcc0c53ec8ca31e909aa81d8ee
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-07-18 14:11:02 +0100

    converted singleCommand and removed guava preconditions and replaced with our own

---
+++ src/main/java/io/airlift/airline/Preconditions.scala
@@ -0,0 +1,24 @@
+package io.airlift.airline
+
+import javax.annotation.Nullable
+
+
+object Preconditions {
+  def checkNotNull[T](reference: T, @Nullable errorMessage: String): T = {
+    if (reference == null) {
+      throw new NullPointerException(String.valueOf(errorMessage))
+    }
+    else {
+      reference
+    }
+  }
+
+  def checkNotNull[T](reference: T, @Nullable errorMessageTemplate: String, @Nullable errorMessageArgs: AnyRef*): T = {
+    if (reference == null) {
+      throw new NullPointerException(errorMessageTemplate.format(errorMessageArgs))
+    }
+    else {
+      reference
+    }
+  }
+}
